209e660f514bdc0efa913bb515205423729e16f9
Fernando Silveira <88800990+huntercodecamp@users.noreply.github.com>
2021-10-18 15:51:09 -0300

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -3,5 +3,5 @@ sudo apt update -y &&
 echo "update foi realizado com sucesso " &&
 sudo apt upgrade -y && 
 echo "upgrade foi realizado com sucesso" &&
-sudo apt install apache2 bind9 dnsutils openssh-server openssh-client whois net-tools systemctl && 
-echo "instalação do "
+sudo apt install apache2 bind9 dnsutils openssh-server openssh-client whois net-tools && 
+echo "instalação dos pacotes (apache2 bind9 dnsutils openssh-server openssh-client whois net-tools) concluida "
